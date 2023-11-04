1aba03fab8eeb04b972917bd06d18b04dd31ba2f
john <huangdi_c@163.com>
2023-11-05 00:38:26 +0800

    feature: 创建欢迎界面的页面

--- src/config/routers.tsx
+++ src/config/routers.tsx
@@ -1,7 +1,23 @@
+import { RouteRecordRaw } from "vue-router";
 import { Bar } from "../views/Bar";
 import { Foo } from "../views/Foo";
+import { First } from "../components/welcome/first";
+import { Second } from "../components/welcome/second";
+import { Third } from "../components/welcome/third";
+import { Forth } from "../components/welcome/forth";
+import { Welcome } from "../views/Welcome";
 
-export const routes = [
-  {path: '/', component: Foo},
-  {path: '/about', component: Bar},
+export const routes: RouteRecordRaw[] = [
+  { path: '/', component: Foo },
+  { path: '/about', component: Bar },
+  {
+    path: '/welcome',
+    components: Welcome,
+    children: [
+      { path: '1', component: First },
+      { path: '2', component: Second },
+      { path: '3', component: Third },
+      { path: '4', component: Forth },
+    ]
+  },
 ]
---
+++ src/views/Welcome.tsx
@@ -0,0 +1,10 @@
+import { defineComponent } from 'vue';
+import { RouterView } from 'vue-router';
+
+export const Welcome = defineComponent({
+  setup: (props, context) => {
+    return () => (
+      <div><RouterView /></div>
+    )
+  }
+})
