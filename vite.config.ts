ac25ae10af6aa2752a5b058cbabeb28b29c216f4
john <huangdi_c@163.com>
2023-11-04 21:43:55 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/mangosteen/', //build path
+  base: '/mangosteen-1-page/dist/', //build path
   plugins: [vue()]
 })
