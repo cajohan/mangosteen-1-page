5b2afd15a978a424dc714ed2d7067313b8e23f97
john <huangdi_c@163.com>
2023-11-05 21:19:11 +0800

    feat：修改默认路由

--- src/config/routers.tsx
+++ src/config/routers.tsx
@@ -1,6 +1,4 @@
 import { RouteRecordRaw } from "vue-router";
-import { Bar } from "../views/Bar";
-import { Foo } from "../views/Foo";
 import { First } from "../components/welcome/First";
 import { Second } from "../components/welcome/Second";
 import { Third } from "../components/welcome/Third";
@@ -8,12 +6,12 @@ import { Forth } from "../components/welcome/Forth";
 import { Welcome } from "../views/Welcome";
 
 export const routes: RouteRecordRaw[] = [
-  { path: '/', component: Foo },
-  { path: '/about', component: Bar },
+  { path: '/', redirect: '/welcome' },
   {
     path: '/welcome',
     component: Welcome,
     children: [
+      { path: '', redirect: '/welcome/1' },
       { path: '1', component: First },
       { path: '2', component: Second },
       { path: '3', component: Third },
--- src/views/Foo.tsx
+++
@@ -1,8 +0,0 @@
-import { defineComponent } from 'vue';
-export const Foo = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <div>FooFk</div>
-    )
-  }
-})
