ee3376a49b3416dabb902d57b1080103e2eed80f
john <huangdi_c@163.com>
2023-11-05 22:21:14 +0800

    feat: 四个欢迎页面

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,8 +1,22 @@
 import { defineComponent } from 'vue';
+import s from './First.module.scss'
+import cloud from '../../assets/icons/cloud.svg'
+import { RouterLink } from 'vue-router';
+
 export const Forth = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <div>Forth</div>
-    )
-  }
+  setup: (props, context) => {
+    return () => (
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img src={cloud} />
+          <h2>云备份<br/>再也不怕数据丢失</h2>
+        </div>
+        <div class={s.actions}>
+          <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+          <RouterLink to="/start" >完成</RouterLink>
+          <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+        </div>
+      </div>
+    )
+  }
 })
