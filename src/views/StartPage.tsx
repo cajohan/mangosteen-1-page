28020047bd84cb7e11aaace557f707299fd806e7
john <huangdi_c@163.com>
2023-11-06 17:16:43 +0800

    feat: 添加Navbar组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -4,6 +4,7 @@ import s from './StartPage.module.scss';
 import { FloatButton } from '../shared/FloatButton';
 import { Center } from '../shared/Center';
 import { Icon } from '../shared/Icon';
+import { Navbar } from '../shared/Navbar';
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
@@ -12,12 +13,17 @@ export const StartPage = defineComponent({
     }
     return () => (
       <div>
-        <nav>menu</nav>
+         <Navbar>{
+          {
+            default: '山竹记账',
+            icon: <Icon name="menu" class={s.navIcon}/>
+          }
+        }</Navbar>
         <Center class={s.pig_wrapper}>
           <Icon name="pig" class={s.pig} />
         </Center>
         <div class={s.button_wrapper}>
-          <Button class={s.button} onClick={onClick}>测试</Button>
+          <Button class={s.button} onClick={onClick}>开始记账</Button>
         </div>
         <FloatButton iconName='add' />
       </div>
