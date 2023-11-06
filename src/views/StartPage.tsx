6444d7cc9771999881daf747ccb9285966995c2c
john <huangdi_c@163.com>
2023-11-06 18:15:34 +0800

    feat: 创建 MainLayout 布局

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -7,6 +7,7 @@ import { Icon } from '../shared/Icon';
 import { Navbar } from '../shared/Navbar';
 import { Overlay } from '../shared/Overlay';
 import { RouterLink } from 'vue-router';
+import { MainLayout } from '../layouts/MainLayout';
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
@@ -15,28 +16,28 @@ export const StartPage = defineComponent({
       refOverlayVisible.value = !refOverlayVisible.value
     }
     return () => (
-      <div>
-         <Navbar>{
-          {
-            default: () => '山竹记账',
-            icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
-          }
-        }</Navbar>
-        <Center class={s.pig_wrapper}>
-          <Icon name="pig" class={s.pig} />
-        </Center>
-        <div class={s.button_wrapper}>
-        <RouterLink to="/items/create">
-            <Button class={s.button}>开始记账</Button>
-          </RouterLink>
-        </div>
-        <RouterLink to="/items/create">
-          <FloatButton iconName='add' />
-        </RouterLink>
-        {refOverlayVisible.value &&
-          <Overlay onClose={() => refOverlayVisible.value = false} />
+      <MainLayout>{
+        {
+          title: () => '山竹记账',
+          icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />,
+          default: () => <>
+            <Center class={s.pig_wrapper}>
+              <Icon name="pig" class={s.pig} />
+            </Center>
+            <div class={s.button_wrapper}>
+              <RouterLink to="/items/create">
+                <Button class={s.button}>开始记账</Button>
+              </RouterLink>
+            </div>
+            <RouterLink to="/items/create">
+              <FloatButton iconName='add' />
+            </RouterLink>
+            {refOverlayVisible.value &&
+              <Overlay onClose={() => refOverlayVisible.value = false} />
+            }
+          </>
         }
-      </div>
+      }</MainLayout>
     )
   }
 })
