eba018920e86e651f257d9f84ed2ee20e0d64270
john <huangdi_c@163.com>
2023-11-10 23:32:39 +0800

    feat: 完成 TagEdit 页面的样式

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,11 @@
 import { defineComponent, PropType } from 'vue';
 import { Navbar } from '../shared/Navbar';
+import s from './MainLayout.module.scss';
 export const MainLayout = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>
-        <Navbar>{
+      <div class={s.wrapper}>
+      <Navbar class={s.navbar}>{
           {
             default: () => context.slots.title?.(),
             icon: () => context.slots.icon?.(),
