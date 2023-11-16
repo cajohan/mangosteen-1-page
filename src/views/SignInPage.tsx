26027b4194dccd03a2d5c711a6f157026ab9ccd7
john <huangdi_c@163.com>
2023-11-16 16:36:05 +0800

    feat: 登录成功后保存 jwt 至 localStorage

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -8,6 +8,7 @@ import s from './SignInPage.module.scss';
 import axios from 'axios';
 import { http } from '../shared/Http';
 import { userBool } from '../hooks/useBool';
+import { history } from '../shared/history';
 export const SignInPage = defineComponent({
   setup: (props, context) => {
     const formData = reactive({
@@ -31,7 +32,9 @@ export const SignInPage = defineComponent({
         { key: 'code', type: 'required', message: '必填' },
       ]))
       if(!hasError(errors)){
-        const response = await http.post('/session', formData)
+        const response = await http.post<{jwt:string}>('/session', formData)
+        localStorage.setItem('jwt', response.data.jwt)
+        history.push('/')
       }
     }
     const onError = (error: any)=> {
