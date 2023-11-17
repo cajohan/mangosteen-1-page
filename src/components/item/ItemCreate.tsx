95a0ea60a1f803f05c7ce750dbb801c2ac45b9f9
john <huangdi_c@163.com>
2023-11-17 17:34:41 +0800

    feat: 收集了四个字段 kind、tag_id、happen_at 和 amount

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -26,6 +26,9 @@ export const ItemCreate = defineComponent({
         _mock: 'tagIndex'
       })
     })
+    const refTagId = ref<number>()
+    const refHappenAt = ref<string>(new Date().toISOString())
+    const refAmount = ref<number>(0)
     return () => (
       <MainLayout class={s.layout}>{{
         title: () => '记一笔',
@@ -34,14 +37,18 @@ export const ItemCreate = defineComponent({
           <div class={s.wrapper}>
             <Tabs v-model:selected={refKind.value} class={s.tabs}>
               <Tab name="支出">
-                <Tags kind="expenses"/>
+                {refAmount.value}
+                <Tags kind="expenses" v-model:selected={refTagId.value} />
               </Tab>
               <Tab name="收入">
-                <Tags kind="income"/>
+                <Tags kind="income" v-model:selected={refTagId.value} />
               </Tab>
             </Tabs>
             <div class={s.inputPad_wrapper}>
-              <InputPad />
+              <InputPad
+                v-model:happenAt={refHappenAt.value}
+                v-model:amount={refAmount.value}
+               />
             </div>
           </div>
         </>
