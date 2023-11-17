5333c298aea94f730c42a701011927cceb3c9b5f
john <huangdi_c@163.com>
2023-11-17 11:55:25 +0800

    test: 删除测试代码

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -36,7 +36,7 @@ export const SignInPage = defineComponent({
         { key: 'code', type: 'required', message: '必填' },
       ]))
       if(!hasError(errors)){
-        const response = await http.post<{jwt:string}>('/session', formData,{params: { _mock: 'session'}}).catch(onError)
+        const response = await http.post<{jwt:string}>('/session', formData).catch(onError)
         console.log(response)
         localStorage.setItem('jwt', response.data.jwt)
         const returnTo = route.query.return_to?.toString()
