9d6e6edb2479a14d026d9c590d12389e8ccde36b
john <huangdi_c@163.com>
2023-11-20 00:03:22 +0800

    feat: 封装 Datetime

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -4,6 +4,7 @@ import { FloatButton } from '../../shared/FloatButton'
 import { http } from '../../shared/Http'
 import s from './ItemSummary.module.scss'
 import { Money } from '../../shared/Money'
+import { Datetime } from '../../shared/Datetime'
 export const ItemSummary = defineComponent({
   props: {
     startDate: {
@@ -62,7 +63,7 @@ export const ItemSummary = defineComponent({
                       <span class={s.tag}>{item.tags_id[0]}</span>
                       <span class={s.amount}>￥<Money value={item.amount}/></span>
                     </div>
-                    <div class={s.time}>{item.happen_at}</div>
+                    <div class={s.time}><Datetime value={item.happen_at}/></div>
                   </div>
                 </li>
               ))}
