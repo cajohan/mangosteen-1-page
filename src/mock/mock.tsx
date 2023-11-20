ca325afc79eb85b84aa1b721b6bdc8c5380ff7f0
john <huangdi_c@163.com>
2023-11-20 20:55:51 +0800

    feat: 完成饼图

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -5,23 +5,43 @@ type Mock = (config: AxiosRequestConfig) => [number, any]
 
 faker.setLocale('zh_CN');
 
-export const mockItemSummary: Mock = config => {
-  return [200, {
-    "groups": [
-      { "happen_at": "2022-07-18T00:00:00.000+0800", "amount": 100 },
-      { "happen_at": "2022-07-22T00:00:00.000+0800", "amount": 300 },
-      { "happen_at": "2022-07-29T00:00:00.000+0800", "amount": 200 }
-    ],
-    "summary": 600
-  }]
+export const mockItemSummary: Mock = (config) => {
+  if (config.params.group_by === 'happend_at') {
+    return [
+      200,
+      {
+        groups: [
+          { happen_at: '2022-07-18T00:00:00.000+0800', amount: 100 },
+          { happen_at: '2022-07-22T00:00:00.000+0800', amount: 300 },
+          { happen_at: '2022-07-29T00:00:00.000+0800', amount: 200 }
+        ],
+        summary: 600
+      }
+    ]
+  } else {
+    return [
+      200,
+      {
+        groups: [
+          { tag_id: 1, tag: { id: 1, name: '交通' }, amount: 100 },
+          { tag_id: 2, tag: { id: 2, name: '吃饭' }, amount: 300 },
+          { tag_id: 3, tag: { id: 3, name: '购物' }, amount: 200 }
+        ],
+        summary: 600
+      }
+    ]
+  }
 }
 
-export const mockItemIndexBalance: Mock = config => {
-  return [200, {
-    expenses: 9900,
-    income: 9900,
-    balance: 0
-  }]
+export const mockItemIndexBalance: Mock = (config) => {
+  return [
+    200,
+    {
+      expenses: 9900,
+      income: 9900,
+      balance: 0
+    }
+  ]
 }
 
 export const mockItemIndex: Mock = (config) => {
