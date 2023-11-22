a678601274773ffe5720ce69d9d87e167587294d
john <huangdi_c@163.com>
2023-11-22 21:45:52 +0800

    fix: 只有一种删除标签的方式

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -11,21 +11,21 @@ export const TagEdit = defineComponent({
   setup: (props, context) => {
     const route = useRoute()
     const numberId = parseInt(route.params.id!.toString())
-    if(Number.isNaN(numberId)){
-      return ()=> <div>id 不存在</div>
+    if (Number.isNaN(numberId)) {
+      return () => <div>id 不存在</div>
     }
     const router = useRouter()
-    const onError = ()=>{
-      Dialog.alert({ title:'提示',message:'删除失败' })
+    const onError = () => {
+      Dialog.alert({ title: '提示', message: '删除失败' })
     }
-    const onDelete = async (options?: {withItems?: boolean})=>{
+    const onDelete = async (options?: { withItems?: boolean }) => {
       await Dialog.confirm({
-        title:'确认',
-        message:'你真的要删除吗？'
+        title: '确认',
+        message: '你真的要删除吗？'
       })
       await http.delete(`/tags/${numberId}`, {
         with_items: options?.withItems ? 'true' : 'false',
-      }, {_autoLoading: true}).catch(onError)
+      }, { _autoLoading: true }).catch(onError)
       router.back()
     }
     return () => (
@@ -35,10 +35,10 @@ export const TagEdit = defineComponent({
         default: () => <>
           <TagForm id={numberId} />
           <div class={s.actions}>
-          <Button level='danger' class={s.removeTags}
-              onClick={()=>onDelete()}>删除标签</Button>
-            <Button level='danger' class={s.removeTagsAndItems}
-              onClick={()=>onDelete({withItems: true})}>删除标签和记账</Button>
+            <Button 
+              level='danger' class={s.removeTagsAndItems}
+              onClick={() => onDelete({ withItems: true })}
+            >删除标签（对应记账也会被删除）</Button>
           </div>
         </>
       }}</MainLayout>
