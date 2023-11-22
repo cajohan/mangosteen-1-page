df9508e087ec9b7ef407c2562462180381757dd8
john <huangdi_c@163.com>
2023-11-23 04:38:22 +0800

    style: 格式化

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,49 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import styleImport, { VantResolve } from 'vite-plugin-style-import';
+import styleImport, { VantResolve } from 'vite-plugin-style-import'
 // @ts-nocheck
-import { svgstore } from './src/vite_plugins/svgstore';
+import { svgstore } from './src/vite_plugins/svgstore'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id: any) {
-          if (id.includes('echarts')) {
-            return 'echarts';
-          }
-          if (id.includes('mock') || id.includes('faker')) {
-            return 'mock';
-          }
-          if (id.includes('vant')) {
-            return 'vant';
-          }
-          if (id.includes('node_modules')) {
-            return 'vendor';
+export default defineConfig(() => {
+  return {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id: any) {
+            if (id.includes('echarts')) {
+              return 'echarts'
+            }
+            if (id.includes('mock') || id.includes('faker')) {
+              return 'mock'
+            }
+            if (id.includes('vant')) {
+              return 'vant'
+            }
+            if (id.includes('node_modules')) {
+              return 'vendor'
+            }
           }
         }
       }
-    }
-  },
-  plugins: [
-    vue(),
-    vueJsx({
-      transformOn: true,
-      mergeProps: true
-    }),
-    svgstore(),
-    styleImport({
-      resolves: [VantResolve()],
-    }),
-  ],
-  server: {
-    proxy: {
-      '/api/v1': {
-        target: 'http://120.77.206.191:3000/',
+    },
+    plugins: [
+      vue(),
+      vueJsx({
+        transformOn: true,
+        mergeProps: true
+      }),
+      svgstore(),
+      styleImport({
+        resolves: [VantResolve()]
+      })
+    ],
+    server: {
+      proxy: {
+        '/api/v1': {
+          target: 'http://120.77.206.191:3000/'
+        }
       }
     }
   }
