0044999bdce07693b1321637c4899f3f3f4c9506
john <huangdi_c@163.com>
2023-12-04 00:50:17 +0800

    fix: typo

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -27,7 +27,7 @@ export class Http {
   }
 }
 
-export const http = new Http(DEBUG ? 'api/v1' : 'http://120.77.206.191:3000/api/v1')
+export const http = new Http(DEBUG ? 'api/v1' : 'https://www.chenjunhuang.top/api/v1')
 
 http.instance.interceptors.request.use((config) => {
   const jwt = localStorage.getItem('jwt')
