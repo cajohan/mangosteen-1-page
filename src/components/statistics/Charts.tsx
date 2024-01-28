489935cacb0f6cb8040e8b048cce4d52ff9a331c
huangd <huangdi_c@163.com>
2024-01-28 21:48:12 +0800

    fix: 修复折线图没数据的问题

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -32,12 +32,14 @@ export const Charts = defineComponent({
       if(!props.startDate || !props.endDate) { return [] }
       const diff = new Date(props.endDate).getTime() - new Date(props.startDate).getTime()
       const n = diff / DAY + 1
-      return Array.from({length: n}).map((_, i)=>{
+      const data1Value = data1.value.reverse()
+      const newArr:[string,number][] = Array.from({length: n}).map((_, i)=>{
         const time = new Time(props.startDate+'T00:00:00.000+0800').add(i, 'day').getTimestamp()
-        const item = data1.value[0]
-        const amount = item && new Date(item.happen_at+'T00:00:00.000+0800').getTime() === time ? data1.value.shift()!.amount : 0
+        const item = data1Value[0]
+        const amount = item && new Date(item.happen_at+'T00:00:00.000+0800').getTime() === time ? data1Value.shift()!.amount : 0
         return [new Date(time).toISOString(), amount]
       })
+      return newArr
     })
 
     const fetchData1 = async () => {
